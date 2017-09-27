77df6fa6b59d135d4c4b5e2d53c27aa92c06cbea
Sunyam Bagga <sunyambagga@gmail.com>
2017-09-26 23:12:54 -0400

    Update scraping_interviews.py

--- scraping_interviews.py
+++ scraping_interviews.py
@@ -1,32 +1,32 @@
 path = '/Users/sunyambagga/Desktop/Applied ML 551/Project 1/Interviews/'
 
-with open(path+'Interview_OUTPUT.txt', 'a') as w:
-    w.write('<dialog>\n')
+with open(path+'interview.xml', 'a') as w:
+    w.write('<dialog>')
     for k in range(1,7):
         with open(path+'i'+str(k)+'.txt', 'rb') as f:
             l = f.read()
-            w.write('<s>')
             temp = l.splitlines()
             interview = []
             for line in temp:
                 if line == '':
                     continue
                 interview.append(line)
-#            print interview
-        
+
+        #     print interview
+            
+            w.write('\n<s>\n')
             for n,utt in enumerate(interview):
-                
-#                print n
-#                print utt
+#                 print n
+#                 print utt
                 if n%2 == 0:
-                    print "I'm here", utt
                     w.write('<utt uid="1">')
                     w.write(utt)
-                    w.write('</utt>')
+                    w.write('</utt>\n')
                 else:
                     w.write('<utt uid="2">')
                     w.write(utt)
-                    w.write('</utt>')
-    
+                    w.write('</utt>\n')
+                if n != 0 and n%11 == 0:
+                    w.write('</s>\n<s>\n')
             w.write('</s>\n')
     w.write('</dialog>')
